16dc2b39029ee83c613ca626c08ed3744944aa19
github username <github email address>
2019-10-12 17:47:14 -0400

    Updates

---
+++ precache-manifest.7d6b9fb57733135e859fe3ad2922e9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28343e10481c34b3e938dfcc59d815a",
+    "url": "/react-lifecycle-methods/index.html"
+  },
+  {
+    "revision": "740fc7033f11a7456d80",
+    "url": "/react-lifecycle-methods/static/css/main.0e424b10.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/react-lifecycle-methods/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "740fc7033f11a7456d80",
+    "url": "/react-lifecycle-methods/static/js/main.638267b3.chunk.js"
+  },
+  {
+    "revision": "45aad5916c86bf09ae03",
+    "url": "/react-lifecycle-methods/static/js/runtime~main.808233a3.js"
+  },
+  {
+    "revision": "5fb9e95929de02e3c72c84f55b316817",
+    "url": "/react-lifecycle-methods/static/media/lifecyclechart.5fb9e959.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-lifecycle-methods/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-lifecycle-methods/precache-manifest.7d6b9fb57733135e859fe3ad2922e9a3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-lifecycle-methods/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
