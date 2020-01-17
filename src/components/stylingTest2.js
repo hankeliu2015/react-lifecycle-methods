cd8eb051d25ab4e421efcd15505ff1f33c8867fe
github username <github email address>
2020-01-17 00:01:34 -0500

    add another component testing emotion styled

---
+++ src/components/stylingTest2.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import styled from '@emotion/styled';
+
+const HeroDiv = styled.div`
+  border: 1px solid red;
+  padding-lef: 1em;
+  margin-bottom: 5px;
+  h4 {
+    color: blue;
+    text-decoration: underline;
+  }
+  p {
+    color: #fcc300;
+  }
+`;
+
+class EmotionStyled extends Component {
+
+  render() {
+
+    return (
+      <div >
+        <div>
+
+          <h4>The Flash - Emotion Styled</h4>
+          <p>The Flash is a fictional superhero in comic books.</p>
+
+        </div >
+        <div>
+          <h4>Reverse Flash - - Emotion Styled</h4>
+          <p>Reverse Flash is a supervillain in comic books.
+        </p>
+      </div>
+
+      </div>
+    )
+  }
+
+}
+
+export default EmotionStyled;
