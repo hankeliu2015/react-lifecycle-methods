d91c08b132f02e33343fe9fc0109b4ff56cc6b3d
Hank Liu <hanke.liu@gmail.com>
2020-08-19 15:28:43 -0400

    define propTypes for both components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
 
   render() {
     return (
       <div>
+
         <h2>{this.props.todo.id}: {this.props.todo.title}</h2>
       </div>
     )
   }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
 }
 
 export default TodoItem;
