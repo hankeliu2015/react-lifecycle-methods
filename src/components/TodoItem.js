ffbc36fe4d7c768cfc0a3281550a1cdd898a7b0e
Hank Liu <hanke.liu@gmail.com>
2020-08-19 16:19:52 -0400

    react jsx inline style test

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
 
+  getStyle = () => {
+    return {
+      backgroundColor: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : "none"
+    }
+  }
+
   render() {
     return (
       <div>
-
-        <h2>{this.props.todo.id}: {this.props.todo.title}</h2>
+        <h2 style={this.getStyle()}>{this.props.todo.id}: {this.props.todo.title}</h2>
       </div>
     )
   }
+}
+
+const testStyle1 = {
+  backgroundColor: '#f4f4f4'
 }
 
 TodoItem.propTypes = {
